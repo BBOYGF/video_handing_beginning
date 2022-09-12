3d2d6338225f7a071524b4fc4497153144afb8ee
fan <664130988@qq.com>
2022-09-13 00:36:48 +0800

    优化时间

--- videos/heginning.js
+++ videos/heginning.js
@@ -2,7 +2,7 @@ import * as mo from 'movy'
 
 const text = mo.addText("观科技", {x: 0, y: 0, fontSize: 1.5, font: 'gdh', color: "yellow"});
 text.reveal();
-text.moveTo({x: 0, y: 5, t: 1, duration: 5, ease: "elastic"});
+text.moveTo({x: 0, y: 5, t: 0, duration: 2, ease: "elastic"});
 let preX, preY, preZ = 0;
 for (let i = 0; i < 100; i = i + 0.1) {
     if (i < 5) {
@@ -13,7 +13,7 @@ for (let i = 0; i < 100; i = i + 0.1) {
     } else {
         let line = mo.addLine({from: [preX, preY, preZ], to: [i, Math.sin(i), Math.cos(i)], lineWidth: 0.05});
         line.fadeIn({
-            t: "<0.05",
+            t: "<0.01",
         });
     }
 
@@ -22,9 +22,9 @@ for (let i = 0; i < 100; i = i + 0.1) {
     preZ = Math.cos(i);
 }
 
-mo.cameraMoveTo({x: 0, y: 0, z: 0, t: 5, duration: 2})
-mo.cameraMoveTo({ry: -0.5 * Math.PI, t: 5, duration: 2})
-mo.cameraMoveTo({x: 20, y: 0, z: 0, t: 7, duration: 10})
-let circle = mo.addCircle({x: 20, y: 0, z: 0,ry:0.5 * Math.PI,t:0});
+mo.cameraMoveTo({x: 0, y: 0, z: 0, t: 1, duration: 2})
+mo.cameraMoveTo({ry: -0.5 * Math.PI, t: 1, duration: 2})
+mo.cameraMoveTo({x: 10, y: 0, z: 0, t: 3, duration: 5, ease: "elastic"})
+let circle = mo.addCircle({x: 15, y: 0, z: 0, ry: 0.5 * Math.PI, t: 0});
 
 mo.run();
